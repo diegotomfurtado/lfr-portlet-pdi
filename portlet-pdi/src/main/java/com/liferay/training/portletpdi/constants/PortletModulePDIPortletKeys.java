c9b7c0d7ed7070eeb8eacf7cdc5562299fef03b5
“diego.furtado” <diegotomfurtado@gmail.com>
2019-06-19 23:53:36 -0300

    Creating the Liferay portlet

---
+++ portlet-pdi/src/main/java/com/liferay/training/portletpdi/constants/PortletModulePDIPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.training.portletpdi.constants;
+
+/**
+ * @author Diego Furtado
+ */
+public class PortletModulePDIPortletKeys {
+
+	public static final String PortletModulePDI = "portletmodulepdi";
+
+}
